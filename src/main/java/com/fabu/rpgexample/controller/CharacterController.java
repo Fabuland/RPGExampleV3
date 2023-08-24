4e5205e8a07a3e33abe2ee69e1f5bf2f594d6f1b
Fabuland <mrfabuland@gmail.com>
2023-08-25 00:29:58 +0200

    Small changes

--- src/main/java/com/fabu/rpgexample/controller/CharacterController.java
+++ src/main/java/com/fabu/rpgexample/controller/CharacterController.java
@@ -1,35 +1,21 @@
 package com.fabu.rpgexample.controller;
 
-import com.fabu.rpgexample.model.EnemiesModel;
-import com.fabu.rpgexample.model.StatsModel;
-import com.fabu.rpgexample.repository.CharacterRepository;
-import com.fabu.rpgexample.repository.EnemiesRepository;
-import com.fabu.rpgexample.repository.StatsRepository;
 import com.fabu.rpgexample.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.fabu.rpgexample.model.CharacterModel;
-
-import java.util.Optional;
 
 @Controller
 public class CharacterController {
 
     private final CharacterService characterService;
-    private final CharacterRepository characterRepository;
-    private final StatsRepository statsRepository;
-    private final EnemiesRepository enemiesRepository;
     public int enemyRandomId;
     public Long characterId;
     public Model characterEnemyModel;
 
     @Autowired
-    public CharacterController(CharacterRepository characterRepository, StatsRepository statsRepository, EnemiesRepository enemiesRepository, CharacterService characterService) {
-        this.characterRepository = characterRepository;
-        this.statsRepository = statsRepository;
-        this.enemiesRepository = enemiesRepository;
+    public CharacterController(CharacterService characterService) {
         this.characterService = characterService;
     }
 
